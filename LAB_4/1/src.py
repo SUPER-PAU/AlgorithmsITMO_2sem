d8fa162ed4dd22b7126870f1abe0de4b7087e872
semenkaprin <kaprinsemen@gmail.com>
2025-05-06 17:00:20 +0300

    LAB 4 task 1

---
+++ LAB_4/1/src.py
@@ -0,0 +1,39 @@
+from tests import test_performance
+
+def prefix_function(s):
+    pi = [0]
+    for i in range(1, len(s)):
+        k = pi[i - 1]
+        while k > 0 and s[i] != s[k]:
+            k = pi[k - 1]
+        if s[i] == s[k]:
+            k += 1
+        pi.append(k)
+    print(pi)
+    return pi
+
+def kmp(p, t):
+    s = p + "#" + t
+    pi = prefix_function(s)
+    result = []
+    len_p = len(p)
+    len_t = len(t)
+    for i in range(1, len_t):
+        if pi[len_p + i + 1] == len_p:
+            result.append(i - len_p + 2)
+    return result
+
+
+@test_performance
+def main():
+    with open("test1.txt", 'r') as file:
+        p = file.readline().strip()
+        t = file.readline().strip()
+
+    res = kmp(p, t)
+    print(len(res))
+    print(res)
+
+if __name__ == "__main__":
+    main()
+    
